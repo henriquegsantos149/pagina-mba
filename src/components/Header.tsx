20f03fb33317aa2b5ab3442d8e07df02d512e346
olivio-pro <olivio@ambientalpro.com.br>
2026-04-17 15:26:30 -0300

    alteração de logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
             <img 
               src={`${import.meta.env.BASE_URL}logo.png`} 
               alt="MBA IDA Logo" 
-              className="h-14 md:h-20 w-auto object-contain py-1 drop-shadow-[0_0_15px_rgba(255,255,255,0.1)]"
+              className="h-12 md:h-16 w-auto object-contain py-1 drop-shadow-[0_0_15px_rgba(255,255,255,0.1)]"
             />
           </a>
         </div>
